d73ad8ee0081c0d99b43832f6653f3d850269209
David Froger <david.froger@thrasos.ai>
2022-08-12 21:36:40 +0200

    set up api-service

--- src/routes/products/products.ts
+++ src/routes/products/products.ts
@@ -1,4 +1,15 @@
 import { ICustomElementViewModel } from 'aurelia';
 
+import { ApiService } from '../../services/api-service';
+
 export class Products implements ICustomElementViewModel {
+    private products = [];
+
+    constructor(private api: ApiService) {
+    }
+
+    async binding(): Promise<any> {
+        this.products = await this.api.getProducts();
+        console.log("===========>", this.products);
+    }
 }
